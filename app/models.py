31f214d6728d85da2d159f01065c72e290a1bb64
oodennis20 <oodennis20@gmail.com>
2019-11-23 12:32:42 +0300

    initial migration done successfully

--- app/models.py
+++ app/models.py
@@ -3,9 +3,24 @@ from . import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(250))
+    author = db.Column(db.String(250))
+    pitches_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     email = db.Column(db.String(250),unique = True,index = True)
 
     def __repr__(self):
          return f'User {self.username}'
     
+class Pitches(db.Model):
+    __tablename__='pitches'
+    id = db.Column(db.Integer,primary_key = True)
+    category = db.Column(db.String(200))
+    pitch = db.Column(db.String(200))
+    time = db.Column(db.String(200))
+    users = db.relationship('User',backref = 'pitches',lazy="dynamic")
+     
+    def __repr__(self):
+        return f'User {self.name}'
+     
+
+
+
