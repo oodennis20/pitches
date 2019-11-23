543a7ce681a8715233613f98b77d6b5d7ed7b973
oodennis20 <oodennis20@gmail.com>
2019-11-23 15:58:25 +0300

    pitch saving

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required,current_user
 from ..models import Pitches, User
 from . import main
 from .. import db
-
+from .forms import PitchForm
 
 @main.route('/')
 def index():
@@ -15,24 +15,26 @@ def index():
     message= "Hello"
     return render_template('index.html', message=message)
 
-@main.route('/pitch', methods = ['GET','POST'])
+@main.route('/pitch/', methods = ['GET','POST'])
 @login_required
 def new_pitch():
+    
     form = PitchForm()
+    
     if form.validate_on_submit():
         category = form.category.data
         pitch= form.pitch.data
 
 
         # Updated pitchinstance
-        new_pitch = Pitch(category= category,pitch= pitch)
+        new_pitch = Pitches(category= category,pitch= pitch)
 
         title='New Pitch'
 
         # save review method
         new_pitch.save_pitch()
 
-        return render_template('pitch.html', title=title, pitch_entry= form)
+    return render_template('pitch.html',pitch_entry= form)
 
 
 
